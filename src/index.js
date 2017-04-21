c5058e035e774fb8594c9395edbba1b1693bd2c1
Carl Littke <carl.littke@gmail.com>
2017-04-21 19:28:53 +0200

    If providing a map between GH and CH userIds comments will belong to the right person on clubhouse and the user who created the issue in Github will be set as the requester. Also harmonize P1-4 labels to always be uppercase P.

--- src/index.js
+++ src/index.js
@@ -35,29 +35,60 @@ function _issueToStory(authorId, projectId, issue, issueComments) {
   return {
     project_id: projectId,
     name: issue.title,
-    description: issue.body,
+    description: `${issue.body}\n\n----------\n[Original Github Issue](${issue.html_url})`,
     comments: _presentComments(authorId, issueComments),
     created_at: issue.created_at,
     updated_at: issue.updated_at,
     external_id: issue.url,
-    labels: _ghToChLabels(issue.labels)
+    labels: _ghToChLabels(issue.labels),
+    story_type: 'bug',
+    requested_by_id: _githubToClubhouseUser(issue.user.id)
   }
 }
 
 function _presentComments(authorId, issueComments) {
-  return issueComments.map(issueComment => ({
-    author_id: authorId,
-    text: `**[Comment from GitHub user @${issueComment.user.login}:]** ${issueComment.body}`,
+  const comments = issueComments.map(issueComment => ({
+    author_id: _githubToClubhouseUser(issueComment.user.id),
+    text: _getCommentText(issueComment),
     created_at: issueComment.created_at,
     updated_at: issueComment.updated_at,
     external_id: issueComment.url,
   }))
+  return comments
 }
 
 function _ghToChLabels(githubLabels) {
   const labels = githubLabels.map(githubLabel => ({
-    name: githubLabel.name
+    name: _convertToUppercaseP(githubLabel.name)
   }))
+
   labels.push({name: `repo:${process.env.GITHUB_REPO_NAME}` })
   return labels
+}
+
+function _convertToUppercaseP(tagName) {
+  // convert p1 etc to P1
+  const isLowerCasePriorityTag = tagName.search(/^p[1-4]$/);
+  if (isLowerCasePriorityTag) {
+    return tagName.replace('p', 'P');
+  }
+  return tagName
+}
+
+function _githubToClubhouseUser(githubUserId) {
+  // Github id is the key, ch id is the value
+  return {
+  }[githubUserId]
+}
+
+// If we can map the github user to a clubhouse user then the user will be displayed
+// as the author in clubhouse. If we cannot map them then the `Github` user will be the author
+// and we add a prefix to show who really created the comment on github.
+function _getCommentText(issueComment) {
+  const chUserId = _githubToClubhouseUser(issueComment.user.id)
+
+  if (chUserId)
+    return issueComment.body
+
+  return `**[Comment from GitHub user @${issueComment.user.login}:]** ${issueComment.body}`
 }
